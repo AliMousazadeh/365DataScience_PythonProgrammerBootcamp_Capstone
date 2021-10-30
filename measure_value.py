c6d73c55a9ffa7f5edfddd27e60d065537c26130
Ali Mousazadeh <ali.mousazadeh98@gmail.com>
2021-10-30 20:50:57 +0330

    read and resize

--- measure_value.py
+++ measure_value.py
@@ -1,10 +1,20 @@
 import numpy as np
 import cv2
-import matplotlib.pyplot as plt
 
-# reads image 'opencv-logo.png' as grayscale
+
+def dispay_image(img):
+    cv2.imshow('image', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+def resize_image(img, down_points):
+    return cv2.resize(img, down_points, interpolation=cv2.INTER_LINEAR)
+
+
 img = cv2.imread('19.2 capstone_coins.png')
+img_resized = resize_image(img, (500, 500))
 
-cv2.imshow('image', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+dispay_image(img_resized)
+
+print(type(img_resized))
