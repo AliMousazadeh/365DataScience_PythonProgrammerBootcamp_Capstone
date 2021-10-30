b848e8ae4b4f656604bc86dc51f07086b8b1f752
Ali Mousazadeh <ali.mousazadeh98@gmail.com>
2021-10-30 20:39:21 +0330

    open and display image

--- measure_value.py
+++ measure_value.py
@@ -1,4 +1,10 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
 
-print('hello world')
-print('hello world again')
-print('hello once more')
+# reads image 'opencv-logo.png' as grayscale
+img = cv2.imread('19.2 capstone_coins.png')
+
+cv2.imshow('image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
