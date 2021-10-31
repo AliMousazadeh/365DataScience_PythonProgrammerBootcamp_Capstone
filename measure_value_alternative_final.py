718ecd62336c21df3814b704398031752b5c2ee6
Ali Mousazadeh <ali.mousazadeh98@gmail.com>
2021-10-31 15:42:45 +0330

    works except for the 50p

---
+++ measure_value_alternative_final.py
@@ -0,0 +1,64 @@
+import numpy as np
+import cv2
+from numpy.lib.type_check import imag
+import matplotlib.pyplot as plt
+
+
+def zscore(img):
+    return (img - np.mean(img))/np.std(img)
+
+
+file_name = '19.2 capstone_coins.png'
+reduction_ratio = 2
+
+image_gray = cv2.imread(file_name, 0)
+image_color = cv2.imread(file_name, 1)
+
+dim = (np.shape(image_gray)[1] // reduction_ratio,
+       np.shape(image_gray)[0] // reduction_ratio)
+
+image_gray = cv2.resize(image_gray, dim, interpolation=cv2.INTER_LINEAR)
+image_color = cv2.resize(image_color, dim, interpolation=cv2.INTER_LINEAR)
+
+
+image_blurred = cv2.GaussianBlur(image_gray, (11, 11), 0)
+
+
+f, axarr = plt.subplots(2, 2)
+axarr[0, 0].imshow(zscore(image_gray), cmap='gray')
+axarr[0, 1].imshow(image_color)
+axarr[1, 0].imshow(zscore(image_blurred), cmap='gray')
+
+circles = cv2.HoughCircles(image_blurred, cv2.HOUGH_GRADIENT, 1, 100,
+                           param1=35, param2=45, minRadius=20, maxRadius=250)
+
+
+if circles is not None:
+    circles = np.round(circles[0, :]).astype('int')
+
+    for (x, y, r) in circles:
+        cv2.circle(image_color, (x, y), r, (0, 255, 0), 3)
+
+axarr[1, 1].imshow(image_color)
+plt.show()
+
+radius_of_coins_to_find = np.round(
+    np.array([44, 52, 62, 67]) * 2/reduction_ratio)
+value_of_coins = [5, 1, 10, 2]
+
+radius_of_coins_found = circles[:, -1]
+
+radius_of_coins_found = np.reshape(
+    radius_of_coins_found, (np.shape(radius_of_coins_found)[0], 1))
+radius_of_coins_to_find = np.reshape(
+    radius_of_coins_to_find, (np.shape(radius_of_coins_to_find)[0], 1))
+
+distance_matrix = (radius_of_coins_found -
+                   np.transpose(radius_of_coins_to_find))**2
+coin_types = np.argmin(distance_matrix, axis=1)
+print(coin_types)
+
+total_value = 0
+for i in range(np.shape(coin_types)[0]):
+    total_value += value_of_coins[coin_types[i]]
+print(total_value)
